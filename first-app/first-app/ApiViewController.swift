8a47805a8d24e053f9a05c92c5891250d84e67f2
Steph Monette <steph@stephmonette.com>
2016-08-24 15:22:35 -0700

    adding another table view, refactoring colors controller, adding navigation titles

---
+++ first-app/first-app/ApiViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ApiViewController.swift
+//  first-app
+//
+//  Created by Steph Monette on 8/24/16.
+//  Copyright © 2016 Steph Monette. All rights reserved.
+//
+
+import UIKit
+
+class ApiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Muni API"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- first-app/first-app/TableViewController.swift
+++ first-app/first-app/TableViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // Data model: These strings will be the data for the table view cells
-    let apps: [String] = ["Colors", "App2"]
+    let apps: [String] = ["Colors", "App 2", "App 3"]
     
     // cell reuse id (cells that scroll out of view can be reused)
     let cellReuseIdentifier = "cell"
@@ -21,7 +21,7 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.title = "Some apps"
         // Register the table view cell class and its reuse id
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
         
@@ -57,6 +57,9 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
         } else if indexPath.row == 1 {
             print("You tapped cell number \(indexPath.row)." + "Open App 2")
             self.performSegueWithIdentifier("app2", sender: nil)
+        } else if indexPath.row == 2 {
+            print("You tapped cell number \(indexPath.row)." + "Open App 3")
+            self.performSegueWithIdentifier("app3", sender: nil)
         }
 
     }
