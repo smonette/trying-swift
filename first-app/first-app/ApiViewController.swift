0e0288aab072ba07401389a0a8044c34058fbca0
Steph Monette <steph@stephmonette.com>
2016-08-30 15:41:10 -0700

    styling, and refactoring

--- first-app/first-app/ApiViewController.swift
+++ first-app/first-app/ApiViewController.swift
@@ -12,7 +12,9 @@ class ApiViewController: UIViewController {
 
 
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
     
     
 
@@ -35,10 +37,20 @@ class ApiViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        self.title = "Some Rando"
+        fetchRandos()
         uiStyling()
         
-        // Do any additional setup after loading the view, typically from a nib.
-        self.title = "Some Rando"
+
+    }
+
+    
+    
+    
+    
+    // MARK: - API Call
+    func fetchRandos() {
+        
         let myUrl = "https://randomuser.me/api/"
         
         let requestURL: NSURL = NSURL(string: myUrl)!
@@ -61,16 +73,20 @@ class ApiViewController: UIViewController {
                     let lastNameData = (nameData["last"] as? String)?.capitalizedString
                     let fullNameData = firstNameData! + " " + lastNameData!
                     
+                    let loginData = (json["results"] as! NSArray)[0]["login"] as! NSDictionary
+                    let usernameData = loginData["username"] as? String
+                    
+                    let userEmail = (json["results"] as! NSArray)[0]["email"] as! String
+                    
                     let imgData = (json["results"] as! NSArray)[0]["picture"] as! NSDictionary
                     let userImg = imgData["large"] as? String
-
-//                    print(json["results"])
-                    
                     
                     dispatch_async(dispatch_get_main_queue(), {
-
+                        
                         self.nameLabel.text = fullNameData
-
+                        self.usernameLabel.text = usernameData
+                        self.emailLabel.text = userEmail
+                        
                         if let url  = NSURL(string: userImg!), data = NSData(contentsOfURL: url) {
                             self.imageView.image = UIImage(data: data)
                         }
@@ -82,7 +98,7 @@ class ApiViewController: UIViewController {
                 } catch {
                     print("Error with Json: \(error)")
                 }
-
+                
                 
                 
             } else {
@@ -92,11 +108,11 @@ class ApiViewController: UIViewController {
         
         task.resume()
         
+        
     }
-
-    
+    // MARK: - UI Styles
     func uiStyling() {
-            imageView.layer.cornerRadius = 50
+        imageView.layer.cornerRadius = 50
     }
 
 
